41c28921185540056da61629ddbdb132ccc7ad1c
Fernanda Gonzalez <fer.ro.mayela@gmail.com>
2018-07-25 15:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
  let currentUser = "Fernanda Gonzalez"
  let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
  let excitedWelcomeMessage = welcomeMessage.toUpperCase()
- let shortGreeting = "Welcome, "
+ let shortGreeting = "Welcome, " currentUser (0,0)
